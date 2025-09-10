983027b7b7cd68dddc85f8e2ee677407b72844bd
JoseScoto <esc16004@byui.edu>
2025-09-10 00:14:43 -0600

    Week01 Assignment

---
+++ shared/scripts/getdates.js
@@ -0,0 +1,3 @@
+// Get current year and populate the copyright year
+document.getElementById('currentyear').textContent = new Date().getFullYear();
+document.getElementById('lastModified').textContent = `Last modified: ${document.lastModified}`;
