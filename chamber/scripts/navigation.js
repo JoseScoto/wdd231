32f148bde9d4bab891e24b208d4670352fdbb22e
JoseScoto <esc16004@byui.edu>
2025-09-12 19:50:40 -0600

    Directory page - Initial commit

---
+++ chamber/scripts/navigation.js
@@ -0,0 +1,9 @@
+// Store the selected elements we're going to use
+const navbutton = document.querySelector('#ham-button');
+const navlinks = document.querySelector('#nav-bar');
+
+// Toghle the show class off and on
+navbutton.addEventListener('click', () => {
+    navbutton.classList.toggle('show');
+    navlinks.classList.toggle('show');
+})
