3a19eb18e34c725de0f310bfead80da22fad0c46
JoseScoto <esc16004@byui.edu>
2025-09-12 21:05:25 -0600

    Updating json file content

--- chamber/scripts/navigation.js
+++ chamber/scripts/navigation.js
@@ -7,3 +7,44 @@ navbutton.addEventListener('click', () => {
     navbutton.classList.toggle('show');
     navlinks.classList.toggle('show');
 })
+
+const url = 'https://raw.githubusercontent.com/JoseScoto/wdd231/main/chamber/data/members.json';
+
+const cards = document.querySelector('#cards');
+
+async function getBusinessData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    displayBusiness(data.businesses);
+}
+
+const displayBusiness = (businesses) => {
+    businesses.forEach((business) => {
+        const card = document.createElement('section');
+        card.classList.add('business-card');
+
+        const businessName = document.createElement('h2');
+        const businessIcon = document.createElement('img');
+        // Additional details
+        
+
+        // h2 content 
+        businessName.textContent = business.business_name || 'No name';
+
+        // Image portrait information
+        businessIcon.src = business.imageurl || '';
+        businessIcon.alt = `Icon of ${business.business_name || 'business'}`;
+        businessIcon.loading = 'lazy';
+        businessIcon.width = 340;
+        businessIcon.height = 340;
+
+        // Appending elements to card 
+        card.appendChild(businessName);
+        card.appendChild(businessIcon);
+        cards.appendChild(card);
+
+
+    });
+}
+
+getBusinessData();
